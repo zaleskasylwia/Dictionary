af03b25fb70a93bcff0a18ac523826b0a68b0128
sylwia <zaleska.syl@gmail.com>
2017-04-13 21:22:46 +0200

    Add a file with definitions and split it, change into dictionary. Plik CSV is with ready long definition

--- zad2.py
+++ zad2.py
@@ -1,20 +1,28 @@
 import csv
-
-dictionary = {"FUNCTION": ("A series of statements which returns some value to a caller. ", "https://docs.python.org/3/glossary.html#term-function"),
+'''
+""dictionary = {"FUNCTION": ("A series of statements which returns some value to a caller. ", "https://docs.python.org/3/glossary.html#term-function"),
                  "PARAMETR": ("co to parametr", "zrodlo p"),"VARIABLE": ("co to variable", "zrodlo v"), "ARGUMENT": ("co to argument", "zrodlo a"), 
                 "DICTIONARY": ("co to dictionary", "zrodlo d"), "TUPLE": ("co to tuple", "zrodlo t"), 
                 "ASCII table": ("co to ascii", "zrodlo asc"),"MODULE": ("co to module", "zrodlo m"),
                 "LIST": ("co to list", "zrodlo l"),"CONDITIONAL": ("co to conditional", "zrodlo c"),
                 "LOOPS": ("co to loops", "zrodlo lo"),"BUILT-IN": ("co to built-in", "zrodlo b"),
                 "SLOTS": ("co to slots", "zrodlo s"),"GENERATORS": ("co to generators", "zrodlo g"),
-                "CLASSES": ("co to classes", "zrodlo cl")}
+                "CLASSES": ("co to classes", "zrodlo cl")}""'''
 
-with open('dictionary.csv', 'w') as f:  
+
+'''with open('dictionary.csv', 'w') as f:  
     w = csv.DictWriter(f, dictionary.keys())
     w.writeheader()
     w.writerow(dictionary)
     
-   
+dict=[]'''
+
+datafile = open('dictionary.csv')   #reading data from csv file
+database = datafile.readlines()
+datafile.close()
+
+database_list = [line.split(' | ') for line in database]
+dictionary = {line[0]:(line[1],line[2]) for line in database_list}   #changing string data into dictionary
     
     
 
@@ -29,18 +37,20 @@ def search():
 
 #2
 def add():
-    print("Tell me a definition")
-    defintion = input().upper()
-    print("Tell me explenation")
-    explenation = input()
-    print("Tell me source")
-    source = input()
-    new_row = {defintion: (explenation, source)}
-    dictionary.update(new_row) 
-    fd = open('document.csv','a')
-    fd.write(new_row)
-    fd.close()
+    new_dict= []
+    definition = input("Tell me a definition: " '\n').upper()
+    new_dict.append(definition)
+    explenation = input("Type explenation of definition: " '\n')
+    new_dict.append(explenation)
+    source = input('Type source of information: ' '\n')
+    new_dict.append(source)
+    dictionary[new_dict[0]] = (new_dict[1], new_dict[2]) #adding new definition to dictionary
+    datafile = open('dictionary.csv', 'a')  #writing new definition to the csv file
+    datafile.write(' | '.join(new_dict))
+    datafile.close()
     print(dictionary)
+
+
 
 
 
